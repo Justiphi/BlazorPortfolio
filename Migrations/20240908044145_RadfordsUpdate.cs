4cb216d6caa43d076db04cb8a1301c706563e496
Travis Wylie <traviswylie58@gmail.com>
2024-09-08 16:42:01 +1200

    RadfordsUpdate migration

---
+++ Migrations/20240908044145_RadfordsUpdate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorPortfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class RadfordsUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Experience",
+                keyColumn: "ExperienceId",
+                keyValue: 6,
+                column: "EndYear",
+                value: 2024);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Experience",
+                keyColumn: "ExperienceId",
+                keyValue: 6,
+                column: "EndYear",
+                value: null);
+        }
+    }
+}
